77da590a65a10b22ffb90546e1618336c0b44b51
loren1994 <dayan805@163.com>
2019-05-25 11:24:28 +0800

    update

--- pages/loren/loren.js
+++ pages/loren/loren.js
@@ -1,13 +1,12 @@
 // pages/loren/loren.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
 
   },
-  scanTap: function () {
+  scanTap: function() {
     wx.scanCode({
       success: (res) => {
         console.log(res.result)
@@ -15,47 +14,45 @@ Page({
         wx.showModal({
           title: '结果',
           content: res.result,
-          success: function (res) {
+          success: function(res) {
             wx.setClipboardData({
               data: result,
-              success: function (res) {
-              }
+              success: function(res) {}
             })
           }
         })
       }
     })
   },
-  callHimTap: function () {
+  callHimTap: function() {
     wx.makePhoneCall({
       phoneNumber: ''
     })
   },
-  callHerTap: function () {
+  callHerTap: function() {
     wx.makePhoneCall({
-      phoneNumber: ''
+      phoneNumber: '10010'
     })
   },
-  phoneTap: function () {
+  phoneTap: function() {
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         wx.showModal({
           title: '手机信息',
-          content: "品牌:" + res.model
-          + "屏幕密度:" + res.pixelRatio
-          + "屏幕尺寸:" + res.windowWidth + " x " + res.windowHeight
-          + "语言:" + res.language
-          + "微信版本号:" + res.version,
-          success: function (res) {
-          }
+          content: "品牌:" + res.model +
+            "屏幕密度:" + res.pixelRatio +
+            "屏幕尺寸:" + res.windowWidth + " x " + res.windowHeight +
+            "语言:" + res.language +
+            "微信版本号:" + res.version,
+          success: function(res) {}
         })
       }
     })
   },
-  locationTap: function () {
+  locationTap: function() {
     wx.getLocation({
       type: 'gcj02',
-      success: function (res) {
+      success: function(res) {
         var latitude = res.latitude
         var longitude = res.longitude
         wx.openLocation({
@@ -66,21 +63,20 @@ Page({
       }
     })
   },
-  vibrateTap: function () {
+  vibrateTap: function() {
     wx.vibrateShort({
-      complete: {
-      }
+      complete: {}
     })
   },
-  newContactTap: function () {
+  newContactTap: function() {
     wx.addPhoneContact({})
   },
-  bezierTap: function () {
+  bezierTap: function() {
     wx.navigateTo({
       url: '../contact/contact'
     })
   },
-  bleTap: function () {
+  bleTap: function() {
     wx.navigateTo({
       url: '../wifi/wifi'
     })
@@ -90,56 +86,56 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
